ea76d839c25a8c05092e23aa0fefc2bef2eeb286
Aden Sarlat <200608715@student.georgianc.on.ca>
2025-10-19 21:25:27 -0400

    Moved JS file and updated slider variables

---
+++ lab-2/page-color-changer.js
@@ -0,0 +1,6 @@
+let red = document.getElementById("red");
+let green = document.getElementById("green"); //finding the sliders by ids
+let blue = document.getElementById("blue");
+
+// Get the body element
+let body = document.body;
